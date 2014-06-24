d02caa416ea4a270cc45775ed2221c822589ce9b
charmedxi <matthew.evans-4@student.manchester.ac.uk>
2014-06-24 17:00:40 +0100

    Added mesh functions.

--- vfmcpp/mesh.cpp
+++ vfmcpp/mesh.cpp
@@ -9,29 +9,33 @@ using namespace std;
 
 vector <double> MeshLengths(vector <vector <double> > Ring){
 	
-	vector <double> Res;
-	vector <vector <double> > Diff;
-	vector <vector <double> >::iterator begin, current, end, next;
+	// Res stores the difference between point N and N+1 
+	vector <double> Res, Diff;
+	// Iterators to search for N+1
+	vector <vector <double> >::iterator begin, current, end;
+	// Flag starts at 0->N
 	int flag;
 
 	// Loop to calculate distance between (flag)th and (flag+1)th point 
 	for(int i=0;i<Ring.size();i++){
 		// First find the adjacent point by flag rather than position in array
 		flag = int(Ring[i][3]+0.5);
-		begin = Ring.begin()+i; end = Ring.end();
+		begin = Ring.begin(); end = Ring.end();
+		vector <vector <double> >::iterator next;
 		for(current=begin; current!=end; current++){
-			if(flag == Ring.size()){flag = -1;}
-			if(int((*current)[3]+0.5)==flag+1){next = current;}
-			else if(current==(end-1)){current = Ring.begin();}
+			if(flag == 0){flag = Ring.size();}
+			if(int((*current)[3]+0.1)==(flag-1)){next = current; current = end-1;}
 		}
 		/* 	Calculate the difference between a point and its neighbour
 			and add this value to Res */
-		Res.push_back(0);
+		Res.push_back(0); Diff.push_back(0); Diff.push_back(0);
+		Diff.push_back(0);
 		for(int j=0;j<3;j++){
-			Diff[i][j] = Ring[i][j] - (*next)[j];
-			Res[i] += pow(Diff[i][j],2); 
+			Diff[j] = Ring[i][j] - (*next)[j];
+			Res[i] += pow(Diff[j],2); 
 		}
 		Res[i] = sqrt(Res[i]);
+
 	}
 	return Res;
 }
--- vfmcpp/vel.cpp
+++ vfmcpp/vel.cpp
@@ -2,5 +2,20 @@
 // Adapted from CalcVelMaster.m by Paul Walmsley
 
 #include <cmath>
+#include <vector>
 
-vector 
+using namespace std;
+
+vector <vector <double> > Velocity(vector <vector <double> > Ring1, vector <vector <double> > Ring2){
+	// Length of filaments in each ring
+	FilLength1 = MeshLengths(Ring1); FilLength2 = MeshLengths(Ring2);
+
+}
+
+vector <vector <double> > SPrime(vector <vector <double> > Ring, vector <vector <double> > FilLength){
+
+}
+
+vector <vector <double> > S2Prime(vector <vector <double> > Ring, vector <vector <double> > FilLength){
+	
+}
