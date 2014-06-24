d518d2f650e6d4155031325dc9265cfa24ed2e5a
Rory Brown <rory.brown-2@student.manchester.ac.uk>
2014-06-24 18:00:06 +0100

    Working ring description, mesh functions; still to calculate velocities.

--- SingleRing/SingleRing/main.cpp
+++ SingleRing/SingleRing/main.cpp
@@ -1,9 +1,7 @@
-#include<iostream>
-#include<iomanip>
-#include<cmath>
-#include<math.h>
-#include<vector>
-#include<stdio.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include "mesh.h"
 
 using namespace std;
 using std::vector;
@@ -12,6 +10,7 @@ using std::vector;
 	double		kappa = 9.98e-8, a0=1.3e-10, a1=exp(0.5)*a0, alpha=0;
 	const int	N = 100; // number of points on ring
 	double		r0=1e-6; // initial ring radius
+	const double pi = 3.14159265358979323846;
 
 int main(){
 
@@ -22,19 +21,19 @@ int main(){
 	position.push_back(2.0e-6);
 
 	// fill theta from 0 to 2pi
-	const double pi = 3.14159265358979323846;
 	double		 diff=(2*pi)/N;
 	double		 theta[N]; theta[0]=0;
 	for(int i=0; i<N; i++){
 		theta[i+1] = theta[i]+diff;
-		cout << "theta[i]=" << theta[i] << endl;
+//		cout << "theta[i]=" << theta[i] << endl;
 	}
 
 
-	// define a vector describing each point, including positions (and flag for neighbours)
-	vector <vector<double> > Ring;
+	// define a vector describing each point, including positions and flag for neighbours
+	vector < vector <double> > Ring;
 	for(int i=0; i<N; i++){
 		vector<double> row;
+		row.push_back(0.0);
 		row.push_back(0.0);
 		row.push_back(0.0);
 		row.push_back(0.0);
@@ -43,12 +42,45 @@ int main(){
 			Ring[i][0]=position[0]+r0*sin(theta[i]);
 			Ring[i][1]=position[0]+r0*cos(theta[i]);
 			Ring[i][2]=position[2];
+			Ring[i][3]=i;
 		}
 	}
 
-/*	for(int r=0; r<N; r++){
-		cout << "ring element r = " << r << "is:: (" << Ring[r][0] << ",  " << Ring[r][1] << ",  " << Ring[r][2] << ")." << endl;
-	}*/
+	for(int r=0; r<N; r++){
+		cout << "ring element r = " << r << "is:	(" << Ring[r][0] << ",	" << Ring[r][1] << ",	" << Ring[r][2] << ")." << endl;
+	}
+
+	vector <double> FilLength = MeshLengths(Ring);
 
 
+	for(int r=0; r<N; r++){
+		cout << "ring element r = " << r << "is:	" << FilLength[r] << endl;
+	}
+
+	// calculate the timesteps
+	double sum = 0;
+	for (int s=0; s<FilLength.size(); s++){
+		sum += FilLength[s];
+	}
+	double	delta = 4*sum/(3*N);
+	double	maxdt = pow((delta/2),2)/(kappa*log10(delta/(2*pi*a0)));
+	double	dt = maxdt/25; // As in Baggaley, Barenghi PRB, 2010*/
+//	cout << "sum, delta, maxdt, dt are :: " << sum << ",   " << delta << ",  " << maxdt << ",  " << dt << endl;
+
+/*	double	save=100; // how many iterations between saving to file
+
+	double	t=0;
+	int		firstloop=0, count=0, filenum=0,
+	double	Nt = Ring.size();
+
+	if count == save; // saving data to files each 100 steps
+		count=0;
+		filenum=filenum+1;
+		ofstream savefile;
+		savefile.open("Data_"filenum".txt.");
+		savefile << ;
+		savefile.close();*/
+		system("pause");
+	return 0;
 }
+
