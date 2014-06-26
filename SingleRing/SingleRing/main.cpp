79dd5a083e23dadb431e4bbf67f5c6ef0e71dbd0
Rory Brown <rory.brown-2@student.manchester.ac.uk>
2014-06-26 19:08:16 +0100

    Nearly done with NonLocalVel, coefficients and sum to 99 still need work.

--- SingleRing/SingleRing/main.cpp
+++ SingleRing/SingleRing/main.cpp
@@ -1,10 +1,10 @@
+#include "mesh.h"
+#include "vel.h"
+#include "velnl.h"
 #include <iostream>
 #include <cmath>
-#include <vector>
-#include "mesh.h"
 
 using namespace std;
-using std::vector;
 
 	// circulation quantum, core radius, ..., mutual friction
 	double		kappa = 9.98e-8, a0=1.3e-10, a1=exp(0.5)*a0, alpha=0;
@@ -29,7 +29,7 @@ int main(){
 	}
 
 
-	// define a vector describing each point, including positions and flag for neighbours
+	// define a vector describing each point, including positions (and flag for neighbours)
 	vector < vector <double> > Ring;
 	for(int i=0; i<N; i++){
 		vector<double> row;
@@ -46,41 +46,25 @@ int main(){
 		}
 	}
 
-	for(int r=0; r<N; r++){
-		cout << "ring element r = " << r << "is:	(" << Ring[r][0] << ",	" << Ring[r][1] << ",	" << Ring[r][2] << ")." << endl;
-	}
+/*	for(int r=0; r<N; r++){
+		cout << "ring element r = " << r << "is:: (" << Ring[r][0] << ",  " << Ring[r][1] << ",  " << Ring[r][2] << ")." << endl;
+	}*/
 
 	vector <double> FilLength = MeshLengths(Ring);
 
 
-	for(int r=0; r<N; r++){
-		cout << "ring element r = " << r << "is:	" << FilLength[r] << endl;
-	}
+/*	for(int r=0; r<N; r++){
+		cout << "ring element r = " << r << "is:: (" << FilLength[r] << endl;
+	}*/
 
-	// calculate the timesteps
-	double sum = 0;
-	for (int s=0; s<FilLength.size(); s++){
-		sum += FilLength[s];
-	}
-	double	delta = 4*sum/(3*N);
-	double	maxdt = pow((delta/2),2)/(kappa*log10(delta/(2*pi*a0)));
-	double	dt = maxdt/25; // As in Baggaley, Barenghi PRB, 2010*/
-//	cout << "sum, delta, maxdt, dt are :: " << sum << ",   " << delta << ",  " << maxdt << ",  " << dt << endl;
+	vector <vector <double> > SPrime = VelocityNL(Ring);
+	//cout << "s' = " << endl;
+	//for(int r=0; r<N; r++){
+	//	cout << "(" << SPrime[r][0] << ", " << SPrime[r][1] << ", " << SPrime[r][2] << ")" << endl;	
+	//}
 
-/*	double	save=100; // how many iterations between saving to file
-
-	double	t=0;
-	int		firstloop=0, count=0, filenum=0,
-	double	Nt = Ring.size();
-
-	if count == save; // saving data to files each 100 steps
-		count=0;
-		filenum=filenum+1;
-		ofstream savefile;
-		savefile.open("Data_"filenum".txt.");
-		savefile << ;
-		savefile.close();*/
-		system("pause");
+	system("pause");
 	return 0;
+
 }
 
