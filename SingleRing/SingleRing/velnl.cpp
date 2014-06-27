a054c9641819b4d57d0a9c5f18a97bee12251eb8
Rory Brown <rory.brown-2@student.manchester.ac.uk>
2014-06-27 18:41:11 +0100

    Now calculates coefficients and q correctly, p only works for k=0.

--- SingleRing/SingleRing/velnl.cpp
+++ SingleRing/SingleRing/velnl.cpp
@@ -4,7 +4,6 @@
 #include "velnl.h"
 #include <vector>
 #include <cmath>
-#include <numeric>
 #include <iostream>
 
 using namespace std;
@@ -23,124 +22,141 @@ vector <vector <double> > VelocityNL(vector <vector <double> > Ring){
 //		vector < vector < vector <double> > > A_temp,B_temp,C_temp,D_temp;
 		vector < vector <double> > p; // p = s_l - s_k
 		vector < vector <double> > q; // q = s_l+1 - s_l
-		
+				
 		if(k==0){
-			for(int l=1;l<L;l++){ 
+			for(int l=1;l<L-1;l++){ 
 				//cout << "p:		" << k << ", " << l << endl;
 				p.push_back(blank);
-				for(int m=0;m<3;m++){p[k][m] = (Ring[l][m]-Ring[k][m]);
+					for(int m=0;m<3;m++){
+					p[l-1][m] = (Ring[l][m]-Ring[k][m]);
 				}
 			}
 		}
 
-		else{	for(int l=0;l<k;l++){
+		//else{	for(int l=1;l<L;l++){
+		//			p.push_back(blank);
+		//			if(l==k){ cout << l << ",	" << k << endl;
+		//			}
+		//			else{
+		//			//cout << "k:	" << k << ", l: " << l << endl;
+		//			
+		//			for(int m=0;m<3;m++){
+		//				p[l-1][m] = (Ring[l][m]-Ring[k][m]);
+		//			//	cout << k << ", " << l << ", " << m << "	" << p[l][m] << endl;
+		//			}
+		//			}
+		//		}
+				/*for(int l=k+1;l<L;l++)
 					//cout << "p:		" << k << ", " << l << endl;
 					p.push_back(blank);
-					for(int m=0;m<3;m++){p[k][m] = (Ring[l][m]-Ring[k][m]);
+					for(int m=0;m<3;m++){
+						p[k][m] = (Ring[l][m]-Ring[k][m]);
 					}
-				}
-				for(int l=k+1;l<L;l++){
-					//cout << "p:		" << k << ", " << l << endl;
-					p.push_back(blank);
-					for(int m=0;m<3;m++){p[k][m] = (Ring[l][m]-Ring[k][m]);
-					}
-				}
-		} // End filling p
+				}*/
+		//} // End filling p
+		
+		for(int l=0;l<L;l++){
+		cout << "k: " << k << ",	l: " << l << ",	p[l][0]: " << p[l][0] << ",	p.size(): " << p.size() << endl;
+		}
+	}
 
-		if(k==L-1)	for(int l=0;l<L-1;l++){
-						//cout << "q:		" << k << ", " << l << endl;
-						q.push_back(blank);
-						for(int m=0;m<3;m++){q[k][m] = (Ring[l+2][m]-Ring[k][m]);
-						}
-					}
+		//if(k==L-1)	for(int l=0;l<L-1;l++){
+		//				//cout << "q:		" << k << ", " << l << endl;
+		//				q.push_back(blank);
+		//				for(int m=0;m<3;m++){q[k][m] = (Ring[l+1][m]-Ring[l][m]);
+		//				
+		//				}
+		//			}
 
-		else if(k==0) for(int l=1;l<L;l++){
-						// cout << "q:		" << k << ", " << l << endl;
-						q.push_back(blank);
-						for(int m=0;m<3;m++){q[k][m] = (Ring[l][m]-Ring[l-1][m]);
-						cout << "q" << k << "from " << l << " is	" << q[k][m] << endl;
-						}
-					}
+		//else if(k==0) for(int l=1;l<L;l++){
+		//				// cout << "q:		" << k << ", " << l << endl;
+		//				q.push_back(blank);
+		//				for(int m=0;m<3;m++){q[k][m] = (Ring[l][m]-Ring[l-1][m]);		
+		//				}
+		//			}
 
-		else{	for(int l=0;l<k;l++){
-					//cout << "q:		" << k << ", " << l << endl;
-					q.push_back(blank);
-					cout << "Below k is not good" << endl;
-					for(int m=0;m<3;m++){q[l][m] = (Ring[l][m]-Ring[k][m]);
-					}
-				}
+		//else{	for(int l=0;l<k;l++){
+		//			//cout << "q:		" << k << ", " << l << endl;
+		//			q.push_back(blank);
+		//			cout << q[k].size();
+		//			cout << k << l << endl;
+		//			for(int m=0;m<3;m++){q[k][m] = (Ring[l][m]-Ring[l-1][m]);
+		//			cout << "q" << k << "from " << l << " is	" << q[k][m] << endl;
+		//			}
+		//		}
 
-				for(int l=k+1;l<L;l++){
-					//cout << "q:		" << k << ", " << l << endl;
-					q.push_back(blank);
-					cout << "Above k is good" << endl;
-					for(int m=0;m<3;m++){q[l-1][m] = (Ring[l][m]-Ring[k][m]);
-					}
-				}
+		//		for(int l=k+1;l<L;l++){
+		//			//cout << "q:		" << k << ", " << l << endl;
+		//			q.push_back(blank);
+		//			cout << "Above k is good" << endl;
+		//			for(int m=0;m<3;m++){q[l-1][m] = (Ring[l][m]-Ring[k][m]);
+		//			}
+		//		}
 
-				//	for(int m=0;m<3;m++){q[0][m] = (Ring[0][m]-Ring[k][m]);}
+		//		//	for(int m=0;m<3;m++){q[0][m] = (Ring[0][m]-Ring[k][m]);}
 
-				
-		} // End filling q
+		//		
+		//} // End filling q
 
 		//	cout << "Point " << k << " finished." << endl;
 	
 
 	// Calculate coefficients: A=|p|, B=p.q, C=|q|
-				A.push_back(blank); B.push_back(blank); C.push_back(blank); D.push_back(blank);
-				A_temp.push_back(blank); B_temp.push_back(blank); C_temp.push_back(blank); D_temp.push_back(blank);
-
-				for(int l=0;l<L;l++){
-					for(int j=0;j<3;j++){
-						A_temp[l][j] = p[l][j]*p[l][j];
-						A_temp[l][j] = sqrt(A_temp[l][j]);
-						B_temp[l][j] = p[l][j]*q[l][j];
-						C_temp[l][j] = q[l][j]*q[l][j];
-						C_temp[l][j] = sqrt(C_temp[l][j]);
-
-						cout << "k, l:	" << k << l << "; " << "A_temp, B_temp, C_temp:	" << A_temp[l][j] << ", " << B_temp[l][j] << ", " << C_temp[l][j] << endl;
-						
-						// The total contribution at point k is found by summing over all other points, l
-						A[k][j]=A[k][j]+A_temp[l][j];
-						B[k][j]=B[k][j]+B_temp[l][j];
-						C[k][j]=C[k][j]+C_temp[l][j];
-
-					cout << "A, B, C:	" << A[k][j] << "	" << B[k][j] << "	" << C[k][j] << endl;
-
-	// Expression for coefficient D given by Samuels in Barenghi et al, Quantized Vortex Dynamics and Superfluid Turbulence, Springer (2001)
-					D[k][j] = (kappa/(4*pi))*((A[k][j]+C[k][j])/(A[k][j]*C[k][j]*(A[k][j]*C[k][j]+B[k][j])));
-					}
-				}
-
-	// Calculate the cross product of p and q
-				cross.push_back(blank);
-				cross[k][0] = p[k][1]*q[k][2] - p[k][2]*q[k][1];
-				cross[k][1] = p[k][2]*q[k][0] - p[k][0]*q[k][2];
-				cross[k][2] = p[k][0]*q[k][1] - p[k][1]*q[k][0];
-
-	// Total nonlocal contribution is found by summing over all elements, D.(p cross q)
-
-//		vector < vector <double> > velnl; vector < vector <double> > velnl_temp;
-				velnl_temp.push_back(blank);
-				for(int j=0;j<3;j++){
-					velnl_temp[k][j] = D[k][j]*cross[k][j]; // Contribution of each element
-					cout << "k: j:	" << k << j << "D, cross, temp	" << D[k][j] << "	" << cross[k][j] << "	" << velnl_temp[k][j] << endl;
-				}
-
-			//for(int j=0;j<3;j++){
-			//	velnl[k] = accumulate(velnl_temp[j].begin(), velnl_temp[j].end(), 0); // Total contribution
-			//}
-
-		velnl[k].resize(L,3);
-
-		for(int j=0;j<3;j++){		
-
-			velnl[k][j] = velnl[k][j] + velnl_temp[k][j];
+//				A.push_back(blank); B.push_back(blank); C.push_back(blank); D.push_back(blank);
+//				A_temp.push_back(blank); B_temp.push_back(blank); C_temp.push_back(blank); D_temp.push_back(blank);
+//
+//				for(int l=0;l<L;l++){
+//					for(int j=0;j<3;j++){
+//						A_temp[l][j] = p[l][j]*p[l][j];
+//						A_temp[l][j] = sqrt(A_temp[l][j]);
+//						B_temp[l][j] = p[l][j]*q[l][j];
+//						C_temp[l][j] = q[l][j]*q[l][j];
+//						C_temp[l][j] = sqrt(C_temp[l][j]);
+//
+//						cout << "k, l:	" << k << l << "; " << '\n' << "A_temp, B_temp, C_temp:	" << A_temp[l][j] << ", " << B_temp[l][j] << ", " << C_temp[l][j] << endl;
+//						
+//						// The total contribution at point k is found by summing over all other points, l
+//						A[k][j]=A[k][j]+A_temp[l][j];
+//						B[k][j]=B[k][j]+B_temp[l][j];
+//						C[k][j]=C[k][j]+C_temp[l][j];
+//
+//					cout << "A, B, C:	" << A[k][j] << "	" << B[k][j] << "	" << C[k][j] << endl;
+//
+//	// Expression for coefficient D given by Samuels in Barenghi et al, Quantized Vortex Dynamics and Superfluid Turbulence, Springer (2001)
+//					D[k][j] = (kappa/(4*pi))*((A[k][j]+C[k][j])/(A[k][j]*C[k][j]*(A[k][j]*C[k][j]+B[k][j])));
+//					cout << "D	" << D[k][j] << endl;
+//					}
+//				//}
+//
+//	// Calculate the cross product of p and q
+//				cross.push_back(blank);
+//				cross[k][0] = p[k][1]*q[k][2] - p[k][2]*q[k][1];
+//				cross[k][1] = p[k][2]*q[k][0] - p[k][0]*q[k][2];
+//				cross[k][2] = p[k][0]*q[k][1] - p[k][1]*q[k][0];
+//				cout << cross[k][0] << "	" << cross[k][1] << "	" << cross[k][2] << endl;
+//				}
+//	// Total nonlocal contribution is found by summing over all elements, D.(p cross q)
+//
+////		vector < vector <double> > velnl; vector < vector <double> > velnl_temp;
+//				velnl_temp.push_back(blank);
+//				for(int j=0;j<3;j++){
+//					velnl_temp[k][j] = D[k][j]*cross[k][j]; // Contribution of each element
+//					cout << "k: j:	" << k << j << "D, cross, temp	" << D[k][j] << "	" << cross[k][j] << "	" << velnl_temp[k][j] << endl;
+//				}
+//
+//			//for(int j=0;j<3;j++){
+//			//	velnl[k] = accumulate(velnl_temp[j].begin(), velnl_temp[j].end(), 0); // Total contribution
+//			//}
+//
+//			velnl[k].resize(L,3);
+//
+//			for(int j=0;j<3;j++){		
+//
+//				velnl[k][j] = velnl[k][j] + velnl_temp[k][j];
 			//velnl[k][j] = accumulate(velnl_temp[k].begin(), velnl_temp[k].end(), 0); // Total contribution
 		//cout << "Finished attempt	" << k << ",	velnl and temp" << j << "is	" << velnl[k][j] << velnl_temp[k][j] << endl;
-		}
-	}
+//			}
+		//}
 
 	return velnl;
 }
-		
