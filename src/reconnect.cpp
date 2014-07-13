95fd9488c769adf4dbbdd4e8e33c4e9d232a0a01
Rory-Br <rory.brown-2@student.manchester.ac.uk>
2014-07-14 00:17:47 +0100

    Need to create a new ring using the points involved in self-reconnection.

--- src/reconnect.cpp
+++ src/reconnect.cpp
@@ -12,8 +12,35 @@ void Tangle::Reconnect(double dr){
 	o_b = mTangle.begin(); o_e = mTangle.end();
 	for (c = b; c < e; c++){
 		for (o_c = o_b; o_c < o_e; o_c++){
-			if (o_c == c){ cout << "Self reconnections!" << endl; }
-			else{
+			if (o_c == c){ // Self-reconnection
+				for (int k(0); k < c->mN; k++){
+					for (int l(0); l < c->mN; l++){
+						if (l == k - 1 || l == k || l == k + 1){ continue; }
+						if (sqrt(pow(c->mPoints[k]->mPos[0] - c->mPoints[l]->mPos[0], 2) + pow(c->mPoints[k]->mPos[1] - c->mPoints[l]->mPos[0], 2) + pow(c->mPoints[k]->mPos[2] - c->mPoints[l]->mPos[2], 2)) < dr) {
+							/* reassign the neighbouring pointers for those adjacent to the point of reconnection */
+							c->mPoints[k]->mPrev->mNext = c->mPoints[l]->mNext;
+							c->mPoints[k]->mNext->mPrev = c->mPoints[l]->mPrev;
+							c->mPoints[l]->mNext->mPrev = c->mPoints[k]->mPrev;
+							c->mPoints[l]->mPrev->mNext = c->mPoints[k]->mNext;
+							/* create a new ring containing points in range k+1 to l-1, delete from current */
+							int j(k+1);
+							Point* occ;
+							while (j < l - k + 1){
+								occ = c->mPoints[j];
+								c->mPoints.erase[j];
+								// Copy point ooc to be passed into ring, possibly as vector?
+								occ++;
+							}
+							c->mPoints.erase[k]; c->mPoints.erase[l];
+							int b = 3; 
+							string NewRing = "Ring" + b;
+							Ring NewRing();
+							b++;
+						}
+					}
+				}
+			}
+			else{ // Reconnections involving another filament
 				for (int k(0); k < c->mN; k++){
 					for (int l(0); l < o_c->mN; l++){
 						if (sqrt(pow(c->mPoints[k]->mPos[0] - o_c->mPoints[l]->mPos[0], 2) + pow(c->mPoints[k]->mPos[1] - o_c->mPoints[l]->mPos[0], 2) + pow(c->mPoints[k]->mPos[2] - o_c->mPoints[l]->mPos[2], 2)) < dr) {
@@ -22,12 +49,10 @@ void Tangle::Reconnect(double dr){
 							c->mPoints[k]->mNext->mPrev = o_c->mPoints[l]->mPrev;
 							o_c->mPoints[l]->mPrev->mNext = c->mPoints[k]->mNext;
 							o_c->mPoints[l]->mNext->mPrev = c->mPoints[k]->mPrev;
-							/* delete the connecting points */
 							c->mN--;
 							o_c->mN--;
 							/* copy points from the other filament to the current filament and delete */
 							c->mN = c->mN + o_c->mN;
-							//for (int j(0); j < o_c->mN; j++){
 							int j(0);
 							Point* occ;
 							occ = o_c->mPoints[l];
@@ -42,6 +67,7 @@ void Tangle::Reconnect(double dr){
 								c->mPoints[j]->mNext = c->mPoints[j + 1];
 								j--;
 							}
+							/* delete the connecting points */
 							c->mPoints.erase[k];
 							mTangle.erase(o_c);
 						}
