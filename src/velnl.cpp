5b6959dfff7f2b8340dc296f00a499574d25e7dc
matthew <matthew.evans-4@student.manchester.ac.uk>
2014-07-13 17:34:39 +0100

    Seem to have fixed velnl and mesh adjustment producing infinite values.

--- src/velnl.cpp
+++ src/velnl.cpp
@@ -1,5 +1,5 @@
-// Calculates the non-local contribution to the velocities of points on each ring
-// Adapted from CalcNonmNocalVel.m by Paul Walmsley
+/* Calculates the non-local contribution to the velocities of points on each ring
+   Adapted from CalcNonmNocalVel.m by Paul Walmsley */
 
 #include "filament.h"
 
@@ -9,8 +9,11 @@ using namespace std;
 
 void Filament::CalcVelocitySelfNL(){
 
-	vector <double> p; // p = s_l - s_k
-	p.resize(3);
+	vector <double> q; 		// q = s_l+1 - s_l
+	vector <double> p; 		// p = s_l - s_k
+	vector <double> pxq; 
+	double pp, qq, pq; 		// p.p, q.q, p.q
+	p.resize(3); q = p; pxq = p;
 	
 	vector <Point*>::iterator b, c, e, c2;
 	b = mPoints.begin(); e = mPoints.end();
@@ -18,16 +21,27 @@ void Filament::CalcVelocitySelfNL(){
 	int i(0);
 	/* loop over all points */
 	for(c=b; c!=e; c++){
+		pp = 0; qq = 0; pq = 0;
 		/* assign pointer to next point */		
 		i = 0;
 		c3 = (*c)->mNext;
 		while(i<mN-2){
+			/* calculate p and q */
 			for(int m(0);m<3;m++){
-				p[m] = c3->mPos[m] - (*c)->mPos[m];
+				p[m] = c3->mPos[m] - (*c)->mPos[m]; // does double the calculations it needs to atm
+				q[m] = c3->mNext->mSegLast[m];
+				pp += p[m]*p[m];
+				qq += q[m]*q[m];
+				pq += p[m]*q[m];
 			}
-			(*c)->mVelNL[0] += (kappa*((2*(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2])+(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2])))/(sqrt((p[0]*p[0]+p[1]*p[1]+p[2]*p[2])+(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))+(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2]))) - (2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))/sqrt((p[0]*p[0]+p[1]*p[1]+p[2]*p[2]))) * (p[1]*c3->mNext->mSegLast[2] - p[2]*c3->mNext->mSegLast[1]))/(2*M_PI*(4*(p[0]*p[0]+p[1]*p[1]+p[2]*p[2])*(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2])-(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))*(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))));
-			(*c)->mVelNL[1] += (kappa*((2*(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2])+(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2])))/(sqrt((p[0]*p[0]+p[1]*p[1]+p[2]*p[2])+(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))+(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2]))) - (2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))/sqrt((p[0]*p[0]+p[1]*p[1]+p[2]*p[2]))) * (p[2]*c3->mNext->mSegLast[0] - p[0]*c3->mNext->mSegLast[2]))/(2*M_PI*(4*(p[0]*p[0]+p[1]*p[1]+p[2]*p[2])*(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2])-(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))*(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))));
-			(*c)->mVelNL[2] += (kappa*((2*(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2])+(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2])))/(sqrt((p[0]*p[0]+p[1]*p[1]+p[2]*p[2])+(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))+(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2]))) - (2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))/sqrt((p[0]*p[0]+p[1]*p[1]+p[2]*p[2]))) * (p[0]*c3->mNext->mSegLast[1] - p[1]*c3->mNext->mSegLast[0]))/(2*M_PI*(4*(p[0]*p[0]+p[1]*p[1]+p[2]*p[2])*(c3->mNext->mSegLast[0]*c3->mNext->mSegLast[0]+c3->mNext->mSegLast[1]*c3->mNext->mSegLast[1]+c3->mNext->mSegLast[2]*c3->mNext->mSegLast[2])-(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))*(2*(p[0]*c3->mNext->mSegLast[0]+p[1]*c3->mNext->mSegLast[1]+p[2]*c3->mNext->mSegLast[2]))));	
+			/* calculate pxq */
+			pxq[0] = p[1]*q[2] - p[2]*q[1];
+			pxq[1] = p[2]*q[0] - p[0]*q[2];
+			pxq[2] = p[0]*q[1] - p[1]*q[0];
+			/* assign values to mVelNL */
+			for(int j(0);j<3;j++){
+				(*c)->mVelNL[j] += (pxq[j]*kappa/(8*M_PI*(pp*qq-pq))) * (2*(qq+pq)/sqrt(pp+qq+2*pq) - 2*pq/sqrt(pp));
+			}
 			/* increment pointer to next */
 			c3 = c3->mNext;
 			i++;
