48fe9a9c4616db1614ada26befab360e5210ed18
charmedxi <matthew.evans-4@student.manchester.ac.uk>
2014-07-29 19:14:05 +0100

    Added plots of filament length and resolution.

--- src/N_animate.py
+++ src/N_animate.py
@@ -1,10 +1,51 @@
 import os.path
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
+import matplotlib.gridspec as gridspec
+import pandas as pd
+import matplotlib
+
+pd.options.display.mpl_style = 'default'
+
+font = {'family' : 'monospace',
+        'size'   : 8}
+
+matplotlib.rc('font', **font)
+
+class Line():
+	def __init__(self):
+		self.L = []
+		self.dr = []
+
+	def linelength(self, r):
+		l = []
+		for j in range (len(r)):
+			l.append(0)
+			for k in range(len(r[j])-1):
+				temp = []
+				for q in range(3):
+					temp.append(pow(r[j][k][q]-r[j][k+1][q],2))
+				tempsum = np.sqrt(sum(temp))
+				l[j] += tempsum
+		self.L.append(sum(l))
+
+	def res(self,r):
+		l = []
+		no_points = 0
+		for j in range (len(r)):
+			l.append(0)
+			for k in range(len(r[j])-1):
+				no_points +=1
+				temp = []
+				for q in range(3):
+					temp.append(pow(r[j][k][q]-r[j][k+1][q],2))
+				l[j] += np.sqrt(sum(temp))
+		self.dr.append((4.0/(3.0*no_points)) * sum(l))
 
 class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
@@ -24,13 +65,17 @@ def init():
 		ring.set_3d_properties([])
 	return rings, time_text
 
+
+
 def getfiles(N_f):
 	files = list()
 	times = list()
-	k = 10
+	k = 0
 	j = 0
 	jmax = 0
-	base_filename = '../bin/data/015_09_self/data_'
+	base_filename = '../bin/data/low_t/data_'
+	if len(sys.argv)!=1:
+		base_filename = '../bin/data/' + str(sys.argv[1]) + '/data_'
 	end = False
 	end2 = False
 	while(end==False):
@@ -55,6 +100,7 @@ def getfiles(N_f):
 def animate(i):
 	end = False
 	m = 0
+	L = 0
 	r = []
 	while(end == False):
 		if os.path.isfile(files[i]+'_'+str(m)+'.dat') == True:
@@ -71,10 +117,24 @@ def animate(i):
 				r[m][j] = data[j].split()
 			r[m][-1] = r[m][0]
 			m+=1
-			#ax.relim()
 		else:
 			rings[m].set_data([],[])
 			rings[m].set_3d_properties([])
+			Length.linelength(r)
+			Length.res(r)
+			len_pts[0].set_data(times[0:int(i)],Length.L[0:int(i)])
+			res_pts[0].set_data(times[0:int(i)],Length.dr[0:int(i)])
+
+			len_line.set_yticks([0.95*Length.L[0], Length.L[0], 1.05*Length.L[0]])
+			len_line.set_yticklabels(['%.1f' % (0.97*Length.L[0]*1000000) + ' um', '%.1f' % (Length.L[0]*1000000), '%.1f' % (1.02*Length.L[0]*1000000) + ' um'])
+			len_line.set_ylim(0.95*min(Length.L), 1.25*max(Length.L))
+			len_line.yaxis.tick_left()
+			res_line.set_yticks([0.95*Length.dr[0], Length.dr[0], 1.05*Length.dr[0]])
+			res_line.set_yticklabels(['%.1f' % (0.95*Length.dr[0]*1000000000) + ' nm', '%.1f' % (Length.dr[0]*1000000000), '%.1f' % (1.05*Length.dr[0]*1000000000) + ' nm'])
+			res_line.set_ylim(0.9*min(Length.dr), 1.1*max(Length.dr))
+			for tl in res_line.get_yticklabels():
+  				 tl.set_color('r')
+
 			end = True
 
 	q_biggest = 0
@@ -87,30 +147,56 @@ def animate(i):
 		rings[q].set_data(r[q][:,0], r[q][:,2]-z_av)
 		rings[q].set_3d_properties(r[q][:,1])
 
-	time_text.set_text('time = %.1f' % (times[i]*1e9)+ ' ns / %.1f' % (times[-1]*1e9) +' ns')
-	ax.view_init(10,-130+0.1*i)
+	time_text.set_text('time = %.1f' % (times[i]*1e6)+ ' us / %.1f' % (times[-1]*1e6) +' us')
+	#ax.view_init(10,-130+0.1*i)
 	fig.canvas.draw()
 	plt.draw()
-	return rings, time_text
+	return rings, time_text, res_pts, len_pts
 
 N_f = 10000
-dt = 1.51828e-11
+dt = 1.3856-10
 
+Length = Line()
+gs = gridspec.GridSpec(2, 1,height_ratios=[5,1])
 files, times, jmax = getfiles(N_f)
-fig = plt.figure(figsize=plt.figaspect(2.), facecolor='w', edgecolor='w')
-ax = fig.add_subplot(111, 
+fig = plt.figure(facecolor='w', edgecolor='w',figsize=plt.figaspect(2.))
+ax = fig.add_subplot(gs[0], 
  	aspect='equal',
  	axisbg='w',
  	xticks=[], yticks=[], zticks=[],
  	xticklabels=[], yticklabels=[], zticklabels=[],
-	projection='3d')
+ 	projection='3d')
+ax2 = fig.add_subplot(gs[1], 
+	xticks=[0,times[-1]], xticklabels=[0, '%.1f' % (10000000*times[-1])],
+	xlabel = 't (us)', 
+	#ylabel = 'Filament length (um)',
+	yticks =[], yticklabels = [],
+ 	axisbg='w')
+
+fig.subplots_adjust(hspace=0)
+res_line = ax2.twinx()
+len_line = ax2.twinx()
+res_line.set_ylabel('Resolution (nm)')
+len_line.set_ylabel('Filament length (nm)')
+len_line.yaxis.set_label_position("left")
 rings = []
 colors = plt.cm.Greens(np.linspace(0.8,1, jmax))
+style = '-'
+if len(sys.argv) > 2:
+	style = str(sys.argv[2])
+
+res_pts = []
+len_pts = []
+
+res_pts += res_line.plot([],[], linewidth=4, c='r')
+len_pts += len_line.plot([],[], linewidth=4, c='k')
+
 for k in range (jmax+2):
-	rings += [l for c in colors for l in ax.plot([], [], [],'-', c=c, alpha = 0.9, linewidth=4)]
-time_text = ax.text(0.0, 0.0, 0, '', transform=ax.transAxes, color='k')
+	rings += [l for c in colors for l in ax.plot([], [], [], style, c=c, alpha = 0.9, linewidth=4, markersize=5, markerfacecolor=c, markeredgecolor='b')]
+time_text = ax2.text(1.0, 1.0, '', transform=ax.transAxes, color='k')
 ax.set_xlim3d((-1.5e-6,1.5e-6))
-#plt.axis('off')
+ax2.set_xlim((0,max(times)))
+# ax2.set_ylim((8.4e-7, 8.5e-7))
 ax.set_ylim3d((-1.5e-6,1.5e-6))
 ax.set_zlim3d((-1.5e-6,1.5e-6))
 ax.view_init(10,-130)
@@ -121,7 +207,7 @@ z = Arrow3D([-1.2e-6,-1.2e-6],[-1.2e-6,-0.8e-6],[-1.6e-6,-1.6e-6], mutation_scal
 ax.add_artist(x)
 ax.add_artist(y)
 ax.add_artist(z)
-ani = animation.FuncAnimation(fig, animate, init_func = init, frames = len(files), interval = 1, blit=False)
+ani = animation.FuncAnimation(fig, animate, repeat=False, init_func = init, frames = len(files), interval = 1, blit=False)
 
 #ani.save('../img/015_09.gif', writer='imagemagick', fps=20);
 #ani.save('reconnection.mp4', fps=30, dpi=500)
