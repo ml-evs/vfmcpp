c5131161c2294752db6d70760dc986ddd23348a3
charmedxi <matthew.evans-4@student.manchester.ac.uk>
2014-08-07 18:06:54 +0100

    Added further options.

--- run.sh
+++ run.sh
@@ -1,14 +1,60 @@
 #!/bin/zsh
 
+usage() {
+cat <<EOF
+run.sh
+---------
 
+DESCRIPTION:
+
+Run the vfmcpp code.
+
+USAGE:
+
+	run.sh [OPTIONS] [FILENAME]
+
+OPTIONS:        
+
+	-c
+		Recompile the code.
+	-d
+		Run the code inside the gdb debugger.
+	-h
+		Show usage information.
+
+	Flags cannot be combined.
+
+FILENAME:
+
+	The initial condition file can be specified; defaults to "run.in".
+
+EOF
+}
 
 COMPILE=0
+DEBUG=0
+
+no=$#
+
+while true
+do
+	case "$3" in
+		-c|--compile) COMPILE=1; shift;;
+		-h|--help) usage; shift;;
+		-d|--debug) DEBUG=1; shift;;
+		-*) echo "invalid flag, showing help"; usage; shift;; 
+		*) in=$3; break;;	
+	esac
+done
 
 while true
 do
 	case "$2" in
 		-c|--compile) COMPILE=1; shift;;
-		*) in=$2; break;;
+		-h|--help) usage; shift;;
+		-d|--debug) DEBUG=1; shift;;
+		-*) echo "invalid flag, showing help"; usage; shift;; 
+		*) in=$2; break;;	
 	esac
 done
 
@@ -16,18 +62,12 @@ while true
 do
 	case "$1" in
 		-c|--compile) COMPILE=1; shift;;
-		*) in=$1; break;;
+		-h|--help) usage; shift;;
+		-d|--debug) DEBUG=1; shift;;
+		-*) echo "invalid flag, showing help"; usage; shift;; 
+		*) in=$1; break;;	
 	esac
 done
-
-
-echo $in
-
-
-if [ $# -eq 0 ]; then
-	echo "no arguments provided, input file defaulting to run.in"
-	in="run.in"
-fi
 
 if [ ! -f "$in" ]; then
 	echo "given input file does not exist, exiting..."
@@ -46,9 +86,9 @@ gre='\e[0;36m'
 dir=$(grep "path" $in)
 dir=${dir:5}
 
-if [ ! -d "bin/$dir" ]; then
+if [ ! -d "$dir" ]; then
 	echo "data directory not found, creating...\n\n"
-	mkdir "bin/$dir"
+	mkdir "$dir"
 fi
 
 if [ $COMPILE -eq 1 ]; then
@@ -58,6 +98,8 @@ if [ $COMPILE -eq 1 ]; then
 	echo " ${blu} success!\n\n"
 	cd ".."
 fi
+
+
 
 cd "bin"
 
@@ -78,4 +120,11 @@ echo "  ${gre}#                 ${yel}     Rory Brown & Matthew Evans           
 echo "  ${gre}#                                                                       ${gre}#"
 echo "  ${gre}#########################################################################\n${red}"
 
-./vfmcpp "../$in"
+
+if [ $DEBUG -eq 1 ]; then
+	gdb -ex run --args vfmcpp "../$in"
+fi
+
+if [ ! $DEBUG -eq 1 ]; then
+	./vfmcpp "../$in"
+fi
