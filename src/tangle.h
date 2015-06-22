a82f167fd572ebb374de650661d789ba03820258
charmedxi <matthew.evans-4@student.manchester.ac.uk>
2015-06-22 16:23:14 +0100

    Commented out fromfile for RQ compatibility

--- src/tangle.h
+++ src/tangle.h
@@ -56,7 +56,7 @@ public:
 	int ReconnectionTest();
 	void CalcVelocity();
 	void CalcField();
-	void FromFile(string base); 	// mostly for debug purposes
+//	void FromFile(string base); 	// mostly for debug purposes
 	void Output(string filename, int i, int file_no);
 	const string StringTime() {
     time_t     now = time(0);
