7a02426af8b1dfc321dd9317d3790f7164f3e192
charmedxi <matthew.evans1992@gmail.com>
2015-08-05 14:21:49 +0100

    Log file now contains position of killed loops

--- src/loopkiller.cpp
+++ src/loopkiller.cpp
@@ -7,13 +7,17 @@ bool Tangle::LoopKill(){
 	bool Killed(false);
 	for(unsigned int i(0); i<mTangle.size(); i++){
 		if(mTangle[i]->mN < 6){
+			mLog << StringTime() << "\t" << setw(10) << mStep << ":\t\tkilled small loop at (";
+			mLog << mTangle[i]->mPoints[0]->mPos[0] << ", ";
+			mLog << mTangle[i]->mPoints[0]->mPos[1] << ", ";
+			mLog << mTangle[i]->mPoints[0]->mPos[2] << ")." << endl;
 			for(unsigned int j(0); j<mTangle[i]->mPoints.size(); j++){
 				delete mTangle[i]->mPoints[j];
 			}
 			delete mTangle[i];
 			mTangle.erase(mTangle.begin()+i);
 			Killed = true;
-			mLog << StringTime() << "\t" << setw(10) << mStep << ":\t\tkilled small loop" << endl;
+		
 		}
 	}
 	if(Killed==true) return true;
